2309768df665b4decf8235ab898fb5d27f688299
Miloslav Pojman <mpojman@akamai.com>
2020-12-02 20:29:28 +0100

    First public version.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open("README.md", "r") as fp:
 
 setup(
     name="dlock",
-    version="0.0.dev",
+    version="0.1",
     author="Miloslav Pojman",
     author_email="mpojman@akamai.com",
     description="Locks your Docker dependencies",
