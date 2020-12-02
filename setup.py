379375b1bbab4558d87d3df2a56516ae2d7a6151
Miloslav Pojman <mpojman@akamai.com>
2020-12-02 20:17:04 +0100

    Add information to setup.py.

--- setup.py
+++ setup.py
@@ -14,9 +14,23 @@
 
 from setuptools import find_packages, setup
 
+with open("README.md", "r") as fp:
+    long_description = fp.read()
+
+
 setup(
     name="dlock",
     version="0.0.dev",
+    author="Miloslav Pojman",
+    author_email="mpojman@akamai.com",
+    description="Locks your Docker dependencies",
+    license="Apache License 2.0",
+    url="https://github.com/akamai/dlock",
+    project_urls={
+        "Source Code": "https://github.com/akamai/dlock",
+    },
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     package_dir={"": "src"},
     packages=find_packages("src"),
     python_requires=">=3.7",
@@ -30,11 +44,40 @@ setup(
             "pytest",
         ]
     },
-    zip_safe=False,
-    include_package_data=True,
     entry_points={
         "console_scripts": [
             "dlock=dlock.cli:run",
         ]
     },
+    include_package_data=True,
+    zip_safe=False,
+    keywords=[
+        "Docker",
+        "Dockerfile",
+        "containers",
+        "dependencies",
+        "lock",
+        "locking",
+        "pinning",
+        "security",
+        "digest",
+        "hash",
+    ],
+    classifiers=[
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Security",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Build Tools",
+        "Topic :: Utilities",
+        "Typing :: Typed",
+    ],
 )
